5b6115b18aec14a8a2ed6cf166301d9e14b6c794
Kelebra <ricardokelebra@gmail.com>
2026-03-24 00:27:09 -0600

    fix deply

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { cookieName, verifySession } from "./lib/auth";
+export const runtime = "nodejs";
 
 export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
